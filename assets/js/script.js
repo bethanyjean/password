e65f53191e24872b06538c2ea9fbd8175e7e59dd
Bethany Jean <bethanyjeanbg@gmail.com>
2022-03-19 16:17:52 -0400

    store other attributes

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,19 @@
 var password = [];
 var length = 0;
-var upper = "yes";
-var lower = "yes";
-var number = "yes";
-var special = "yes";
+var upper = "";
+var upperCaseTxt = "upper case letters";
+var lower = "";
+var lowerCaseTxt = "lower case letters";
+var number = "";
+var numberTxt = "numbers";
+var special = "";
+var specialCharTxt = "special characters";
 
 // ask user for the different parameters of the password
 
+// ask for the password and check the length of the password
+
 var passwordLength = function() {
-    // ask for the password and check the length of the password
     length = window.prompt("How long would you like your password?");
     if (length < 8 || length > 128) {
         window.alert("Your password must be between 8 and 128 characters.");
@@ -21,4 +26,27 @@ var passwordLength = function() {
     }
 }
 
+// Choose and verify other attributes
+
+var makeChoices = function(attributeTxt) {
+    var attribute = window.prompt("Do you want your password to contain " + attributeTxt + "? Y/N");
+    if (attribute.toLowerCase() === 'y') {
+        if (confirm("You want to include " + attributeTxt + " in your password")== false) {
+            attribute = 'n'
+        };
+    } else if(attribute.toLocaleLowerCase() === 'n'){
+        if (confirm("You DO NOT want to include " + attributeTxt + " in your password")== false) {
+            attribute = 'y'
+        };
+    } else {
+        (window.alert("Please enter a valid response: Y/N"));
+        makeChoices(attribute, attributeTxt);
+    }
+    return attribute;
+}
+
 passwordLength();
+var upper = makeChoices(upperCaseTxt);
+var lower = makeChoices(lowerCaseTxt);
+var number = makeChoices(numberTxt);
+var special = makeChoices(specialCharTxt);
